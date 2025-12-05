d738e82f79907d36410de05030b311f6f8831f39
MicroCalc <baindannydaniel@gmail.com>
2025-12-05 23:43:38 +0600

    Delete vitest.setup.ts

--- vitest.setup.ts
+++
@@ -1,9 +0,0 @@
-import { expect, afterEach } from 'vitest';
-
-// Clean up after each test
-afterEach(() => {
-  // Clean up any side effects
-});
-
-// Custom matchers can be added here if needed
-
